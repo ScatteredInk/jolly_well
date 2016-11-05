29cfc0b299129c5aa47df6882b77298db6ef23d3
scatteredink <jack@knocale.com>
2016-11-05 17:09:36 +0000

    move api calls and data cleaning into separate script

--- app.R
+++ app.R
@@ -1,88 +1,10 @@
 library(shiny)
 library(shinyjs)
-library(httr)
-library(dplyr)
-library(lubridate)
-library(forcats)
+
+source("twfy_api.R")
 
 #Sys.setenv("BUILD" = "DEVELOP")
 develop_build <- ifelse(Sys.getenv("BUILD")=="DEVELOP", TRUE, FALSE)
-
-key_from_environ <- function(keyname) {
-  key <- Sys.getenv(keyname)
-  if (identical(key, "")) {
-    stop(sprintf("Please set env var %s in your .Renviron file", keyname),
-         call. = FALSE)
-  }
-  key
-}
-
-hansard_search <- function(search_term, key = twfy_api_key, page = "1") {
-  base_hansard_url <- "https://www.theyworkforyou.com"
-  url <- modify_url(base_hansard_url, path = paste("api/getHansard?key=", key,
-                                                   "&search=", search_term,
-                                                   "&page=", page,
-                                                   "&output=js", 
-                                                   collapse = "", sep = ""))
-  
-  resp <- GET(url, ua)
-  
-  #if (http_type(resp) != "application/json") {
-  #  stop("API did not return json", call. = FALSE)
-  #}
-  
-  parsed <- jsonlite::fromJSON(content(resp, "text"), simplifyDataFrame =TRUE)
-  
-  if (http_error(resp)) {
-    stop(
-      sprintf(
-        "They Work For You API request failed [%s]\n%s\n<%s>", 
-        status_code(resp),
-        parsed$message,
-        parsed$documentation_url
-      ),
-      call. = FALSE
-    )
-  }
-  structure(
-    list(
-      content = parsed,
-      path = search_term,
-      response = resp
-    ),
-    class = "they_work_for_you_api"
-  )
-  
-  
-  parsed
-}
-
-get_all_hansard_search <- function(search_term, key = twfy_api_key) {
-  first_result <- hansard_search(search_term, key)
-  num_pages <- ceiling(first_result[[1]]$total_results/20)
-  if (num_pages == 1) return (first_result)
-  
-  hansard_results <- list()
-  hansard_results[[1]] <- first_result[["rows"]]
-  
-  for (page in seq(2, num_pages)) {
-    res <- hansard_search(search_term, key, page = page)
-    hansard_results[[page]] <- res[["rows"]]
-  }
-  
-  hansard_results
-}
-
-process_result_df <- function(df) {
-  speaker_df <- df$speaker %>%
-    select(member_id, house, constituency, 
-           party, name)
-  debate_df <- df %>%
-    select(hdate, extract)
-  extract_df <- df$parent %>%
-    select("debate" = body)
-  as.data.frame(do.call(cbind, c(speaker_df, debate_df, extract_df)))
-}
 
 ua <- user_agent("http://github.com/scatteredink")
 api_query = "%22jolly+well%22"
